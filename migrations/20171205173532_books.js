46f17765e65ad6a8383da78d6dce4d7d8bb25da6
nrfi@sbcglobal.net <scott.m.rushford@gmail.com>
2017-12-06 20:22:53 -0700

    5 tests passed

--- migrations/20171205173532_books.js
+++ migrations/20171205173532_books.js
@@ -1,15 +1,35 @@
-
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable("books", (table) => {
-    table.increments();
-    table.varchar("title", 255).notNullable().defaultTo("");
-    table.varchar("author", 255).notNullable().defaultTo("");
-    table.varchar("genre", 255).notNullable().defaultTo("");
-    table.text("description").notNullable().defaultTo("");
-    table.text("cover_url").notNullable().defaultTo("");
-    table.timestamp("created_at").notNullable("").defaultTo(knex.raw('now()'));
-    table.timestamp("updated_at").notNullable("").defaultTo(knex.raw('now()'));
+  return knex.schema.createTable('books', table => {
+    table.increments()
+    table
+      .string('title')
+      .notNullable()
+      .defaultTo('')
+    table
+      .string('author')
+      .notNullable()
+      .defaultTo('')
+    table
+      .string('genre')
+      .notNullable()
+      .defaultTo('')
+    table
+      .text('description')
+      .notNullable()
+      .defaultTo('')
+    table
+      .text('cover_url')
+      .notNullable()
+      .defaultTo('')
+    table
+      .timestamp('created_at')
+      .notNullable('')
+      .defaultTo(knex.raw('now()'))
+    table
+      .timestamp('updated_at')
+      .notNullable('')
+      .defaultTo(knex.raw('now()'))
   })
-};
+}
 
-exports.down = (knex, Promise) => knex.schema.dropTable("books");
+exports.down = (knex, Promise) => knex.schema.dropTable('books')
